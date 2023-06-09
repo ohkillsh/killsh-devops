831a68fbe7c00f545f49b4b1e74252737dc25a89
Kuno <gustavo.kuno@linx.com.br>
2023-06-09 08:42:19 -0300

    iac: fix variables

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "terraform_infra" {
-  source = "git@github.com:ohkillsh/killsh-module-terraform-base-infra.git"
+  source = "git@github.com:ohkillsh/killsh-module-terraform-base-infra.git?ref=develop"
 
   sp_client_id = "8d864f9e-1b9a-4041-8a40-efdf4c1fd175"
-  sp_object_id = "05d276e2-1ef0-425e-a681-5c51636732c8"
+  sp_object_id = "078af6d6-bb65-44be-97ed-1769d61b178e"
   product      = "killsh"
   environment  = "global"
   location     = "eastus"
