503cb0385bfe68ac20e87781a869bc1aef0c8ec3
Kuno <gustavo.kuno@linx.com.br>
2023-06-09 08:57:28 -0300

    iac: update ref

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "terraform_infra" {
-  source = "git@github.com:ohkillsh/killsh-module-terraform-base-infra.git?ref=develop"
+  source = "git@github.com:ohkillsh/killsh-module-terraform-base-infra.git?ref=main"
 
   sp_client_id = "8d864f9e-1b9a-4041-8a40-efdf4c1fd175"
   sp_object_id = "078af6d6-bb65-44be-97ed-1769d61b178e"
