1458ba852b51a1be01317596b5808f439df3796c
Kuno <gustavo.kuno@linx.com.br>
2023-06-22 19:51:01 -0300

    cf

--- cloudflare.tf
+++ cloudflare.tf
@@ -4,25 +4,28 @@ data "cloudflare_zone" "ohkillsh_win" {
   depends_on = [module.kubernetes_config]
 }
 
+# output "debug" {
+#   value = module.kubernetes_config.ingress_public_ip
+# }
 
 resource "cloudflare_record" "aks_ingress" {
   zone_id = data.cloudflare_zone.ohkillsh_win.id
   name    = "ingress-dev-killsh"
-  value   = module.kubernetes_config.ingress_public_ip
+  value   = "20.253.29.58" #module.kubernetes_config.ingress_public_ip
   type    = "A"
   proxied = true
-  ttl     = 3600
+  ttl     = 1
 
   depends_on = [data.cloudflare_zone.ohkillsh_win]
 }
 
 
 resource "cloudflare_record" "aks_app_web" {
-  zone_id = data.cloudflare_zone.ohkillsh_win.id
-  name    = "app"
-  value   = cloudflare_record.aks_ingress.hostname
-  type    = "CNAME"
-  ttl     = 3600
-
+  zone_id    = data.cloudflare_zone.ohkillsh_win.id
+  name       = "app"
+  value      = cloudflare_record.aks_ingress.hostname
+  type       = "CNAME"
+  ttl        = 1
+  proxied    = true
   depends_on = [data.cloudflare_zone.ohkillsh_win]
 }
